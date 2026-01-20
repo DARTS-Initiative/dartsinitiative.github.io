7f7a4037cee8fb632650fc11129897d5a81299a8
Pablo <pablo.magarinos@outlook.com>
2026-01-20 15:11:40 +0100

    Move footer rendering to JavaScript
    Removed static footer markup from index.html and updated footerRenderer.js to dynamically create and inject the footer element if it does not exist. This change improves flexibility and ensures the footer is always rendered via JavaScript.

--- src/renderers/footerRenderer.js
+++ src/renderers/footerRenderer.js
@@ -1,18 +1,25 @@
 import { config } from '../config.js';
 
 export function renderFooter() {
-    const footerContent = document.querySelector('.footer-content');
-    if (!footerContent) return;
+    let footer = document.querySelector('footer');
+
+    // Create footer if it doesn't exist
+    if (!footer) {
+        footer = document.createElement('footer');
+        document.body.appendChild(footer);
+    }
 
     const linksHtml = config.footer.links.map(link =>
         `<a href="${link.url}" target="${link.url.startsWith('http') ? '_blank' : '_self'}">${link.text}</a>`
     ).join('');
 
-    footerContent.innerHTML = `
-        <p class="copyright">${config.footer.copyright}</p>
-        <p class="license-info">${config.footer.license}</p>
-        <div class="footer-links">
-            ${linksHtml}
+    footer.innerHTML = `
+        <div class="footer-content">
+            <p class="copyright">${config.footer.copyright}</p>
+            <p class="license-info">${config.footer.license}</p>
+            <div class="footer-links">
+                ${linksHtml}
+            </div>
         </div>
-        `;
+    `;
 }
